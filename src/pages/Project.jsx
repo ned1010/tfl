34ed178b08d1a9fe11262e38294fcece58981cb9
Nidup Dorji <nidup1998@gmail.com>
2024-04-20 19:42:19 +0800

    fix image path in project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero-section/Hero";
 import ProjectImg from "@/assets/teach10.jpg";
+// import ProjectImg from "@/assets/teach2.jpg";
 import Footer from "@/components/footer/Footer";
 import Project from "@/components/project/Project";
 import VolunteerLink from "../components/volunteer-form/VolunteerLink";
